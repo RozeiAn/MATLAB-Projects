8b01831f8da195d54634fa44198e0e5d29929721
Adin Rose <lvgolden1@gmail.com>
2026-08-23 11:32:04 -0400

    Implement stellar motion graphs for dedicated stars
    Added part 2 for plotting stellar motion graphs based on speed.

--- RedShift_Aidan_Rosenberg.m
+++ RedShift_Aidan_Rosenberg.m
@@ -34,3 +34,24 @@ speed = z*299792.458; %km/s
 for  i = 1:length(speed)
     disp("Speed of Star " + i + ": " + speed(i) + " km/s");
 end
+
+% Part 2: Stellar Motion Graphs for the Dedicated Stars in "starnames".
+for v = 1:7
+
+    s = spectra(:,v);
+
+    if speed(v) <= 0
+
+        plot(lambda, s, "--");
+
+    else speed(v) > 0
+
+        plot(lambda, s, LineWidth = 3);
+
+    end
+    hold on;
+end
+hold off;
+
+legend(starnames)
+movaway = starnames(speed > 0) %indicates the stars that are moving further from the Earth
